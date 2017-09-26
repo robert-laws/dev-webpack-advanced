321dce84e17a42140d35ce738c573ed30b029bac
Robert Laws <roblaws@me.com>
2017-09-26 21:24:50 +0300

    up to output management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import _ from 'lodash'
+import './style.css'
+import printMe from './print.js'
 
 function component() {
   var element = document.createElement('div')
+  var btn = document.createElement('button')
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
+
+  btn.innerHTML = 'Click here and check the console'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
+
   return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ]
 }
