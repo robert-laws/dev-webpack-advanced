b8af829b70ae594aca9477d8d217392606ccb709
Robert Laws <roblaws@me.com>
2017-09-27 08:31:39 +0300

    code splitting with common chunks plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
+    second: './src/second-module.js'
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
   },
   module: {
     rules: [
@@ -25,7 +30,10 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
+      title: 'Code Splitting'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common'
     })
   ]
 }
